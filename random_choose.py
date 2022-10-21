ff6c212b738cc890351ce9aca95e130be1d39ce3
AskarRgithub <askarrysaliev@gmail.com>
2022-10-21 18:54:22 +0600

    first

---
+++ random_choose.py
@@ -0,0 +1,10 @@
+import random
+from random import shuffle
+
+list_of_students = ['Askar', 'Zhibek', 'Aimeerim', 'Sultanbek', 'Akmal', 'Aktan', 'Nurzhigit','Elhan', 'Akmal']
+shuffle(list_of_students)
+print(list_of_students)
+
+def Randomizer(n):
+    dictionary = {}
+    dictionary.pop()
